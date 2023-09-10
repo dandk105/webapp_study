d4350a5977909e13fab23f62e0ab1001ab52e862
dandk105 <d2cak5s.e43d@gmail.com>
2023-09-10 22:29:23 +0900

    サーバーの起動をログを追加してわかりやすくした

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/dandk105/webapp_study/backend
+
+go 1.21.1
--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/api/greet", greetHandler) // /api/greet へのアクセス時に greetHandler を呼び出す
 
-	http.ListenAndServe(":8080", nil) // 8080ポートでサーバーを起動
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil)) // 8080ポートでサーバーを起動. エラーが発生した時のみログに出力
 }
 
 func greetHandler(w http.ResponseWriter, r *http.Request) {
