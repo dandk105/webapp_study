9ac559e980418fad3387ba9fc57b9d0be9a0101d
dandk105 <d2cak5s.e43d@gmail.com>
2023-09-25 00:51:20 +0900

    DBクライアント初期化のためのコードを追加

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 // グローバルにハンドラのマップを定義
 var routes = make(map[string]string)
 
 // ルートを追加する関数
+// pattern: ルートのパターン
+// description: ルートの説明
+// handlerFunc: ルートにアクセスした時に呼び出されるハンドラ関数
 func addRoute(pattern string, description string, handlerFunc http.HandlerFunc) {
 	routes[pattern] = description
 	http.HandleFunc(pattern, handlerFunc)
 }
 
 func main() {
+	// データベースを初期化
+	db := initDB()
 
 	// /api/greet へのアクセス時に greetHandler を呼び出す
 	addRoute("/api/greet", "Greet API", greetHandler)
@@ -31,7 +38,24 @@ func main() {
 
 }
 
-func greetHandler(w http.ResponseWriter, r *http.Request) {
+// dbクライアントを初期化する関数
+func initDB() *sql.DB {
+	dbUser := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+
+	// データベースに接続するための文字列を生成
+	d := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable", dbUser, dbName, dbPassword, dbHost)
+	db, err := sql.Open("postgres", d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	return db
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	// GETメソッドのみを受け入れる
 	if r.Method != http.MethodGet {
 		http.Error(w, "Only GET method is supported", http.StatusMethodNotAllowed)
@@ -42,6 +66,13 @@ func greetHandler(w http.ResponseWriter, r *http.Request) {
 	if name == "" {
 		name = "World"
 	}
+
+	// データベースからデータを取得する
+	rows, err := db.Query("SELECT message FROM greetings")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
 	response := fmt.Sprintf("Hello, %s!", name)
 	log.Printf("%s request: %s from %s", r.Method, r.RequestURI, r.RemoteAddr)
