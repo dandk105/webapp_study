576ef1d5bc0a63b76bad2b852383e99795864ec9
dandk105 <d2cak5s.e43d@gmail.com>
2023-10-09 23:10:26 +0900

    Finished wiriting code to connect DB

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -22,12 +23,21 @@ func addRoute(pattern string, description string, handlerFunc http.HandlerFunc) 
 	http.HandleFunc(pattern, handlerFunc)
 }
 
+type User struct {
+	// IDはUUIDになるから、stringでまとめてしまうのはいささか乱雑な気がする
+	ID       string
+	Name     string
+	Birthday time.Time
+}
+
 func main() {
 	// データベースを初期化
 	db := initDB()
+	defer initDB()
 
 	// /api/greet へのアクセス時に greetHandler を呼び出す
 	addRoute("/api/greet", "Greet API", greetHandlerWrapper(db))
+	addRoute("/api/status", "Status Check API", statuscheckHandlerWrapper(db))
 
 	// サーバー起動時にハンドラの一覧をログに表示
 	log.Println("Registered routes:")
@@ -50,14 +60,25 @@ func initDB() *sql.DB {
 	dbName := os.Getenv("DB_NAME")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbHost := os.Getenv("DB_HOST")
+	//dbPort := os.Getenv("DB_PORT")
 
 	// データベースに接続するための文字列を生成
+	// TIPS: hostはdocker-compose.ymlで定義したサービス名を指定する必要がある(localhostだと接続できない)
 	d := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable", dbUser, dbName, dbPassword, dbHost)
 	db, err := sql.Open("postgres", d)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
-	defer db.Close()
+
+	// データベースに接続できるか確認
+	// THINK: DBへのpingが失敗した時のステータスを保持する変数がpingErrなのは
+	// ちょっと分かりづらい気がする
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Print(pingErr)
+	}
+	log.Print("Successfully connected!")
+
 	return db
 }
 
@@ -74,20 +95,60 @@ func greetHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	}
 
 	// データベースからデータを取得する
-	rows, err := db.Query("SELECT name FROM USERS WHERE name = $1", name)
+	// TODO: URLのクエリーによって取得するデータを変えるようにする
+	// 現在はUSERSテーブルの全てのデータを取得しているからよろしくない
+	rows, err := db.Query("SELECT * FROM USERS")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.ID, &user.Name, &user.Birthday); err != nil {
+			log.Fatal(err)
+		}
+		users = append(users, user)
+	}
+
+	log.Print(users)
+
+	if err := rows.Err(); err != nil {
+		log.Print(err)
+	}
 
 	response := fmt.Sprintf("Hello, %s!", name)
 	log.Printf("%s request: %s from %s", r.Method, r.RequestURI, r.RemoteAddr)
 	w.Write([]byte(response))
 }
 
+func statuscheckHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only GET method is supported", http.StatusMethodNotAllowed)
+		return
+	}
+
+	dbErr := db.Ping()
+	if dbErr == nil {
+		w.Write([]byte("OK"))
+	} else {
+		http.Error(w, "DB connection error", http.StatusInternalServerError)
+	}
+
+}
+
 // greetHandlerをhttp.HandlerFuncに変換する
+// dbとhttp.HandlerFuncを受け取り、http.HandlerFuncを返す方が
+// 結合度が下がって他のハンドラーにも使うことができそう
 func greetHandlerWrapper(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		greetHandler(w, r, db)
+	}
+}
+
+func statuscheckHandlerWrapper(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		statuscheckHandler(w, r, db)
 	}
 }
