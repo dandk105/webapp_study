9596f568ea2c7830c5bafa948c478fe7d0a63da2
dandk105 <d2cak5s.e43d@gmail.com>
2023-10-22 21:59:04 +0900

    忘れていたファイルを追加

--- frontend/src/views/StatusView.vue
+++ frontend/src/views/StatusView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="status">
     <h1>This is an status page</h1>
-    <button @focus="getData">status is ={{data}}</button>
+    <button @focus="getData">status is</button>
+    <div> = {{ status }}</div>
   </div>
 </template>
 
@@ -26,11 +27,10 @@ export  default {
     async getData() {
       try {
         // TODO: VITE_のプリフィックスをつけないとviteが環境変数を認識しない
-        // corsの設定をしていないので、localhost:3000からlocalhost:8000にアクセスできない
-        // これはブラウザの設定になるので、viteとは別の問題
+        // TIPS: corsの設定はフロントもそうだが、バックエンドでもリソースにアクセスして良い
+        // ということを記述する必要がある
         const base_url:string = import.meta.env.VITE_BASE_API_URL+'/api/status'
         const response = await fetch(base_url)
-        console.log(response)
         if (!response.ok) {
           throw new Error(response.statusText)
         }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "api": "http://localhost:5001"
+      "/api": "http://localhost:5001"
     }
   }
 })
